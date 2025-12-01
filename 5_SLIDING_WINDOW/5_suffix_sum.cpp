79bc485f12001b85dacbc40202b1d6b76d26929b
Deepak kumar <singhdeepak10274@gmail.com>
2025-12-02 01:43:58 +0530

    Add files via upload

---
+++ 5_SLIDING_WINDOW/5_suffix_sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+
+int arr[]={1,2,3,4,5,6};
+int sum=0;
+int n= 6;
+vector<int>ans;
+for(int i=5;i>=0;i--)
+{
+    sum=sum+arr[i];
+    ans.push_back(sum);
+}
+for(int i=0;i<ans.size();i++)
+{
+    cout<<ans[i]<<" ";
+}
+
+
+
+
+    return 0;
+}
